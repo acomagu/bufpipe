608228a0e4613f3fa491838901acc0abe4c04442
Yuki Ito <acomagu@gmail.com>
2019-06-18 16:38:59 +0900

    Add global example

--- bufpipe_example_test.go
+++ bufpipe_example_test.go
@@ -1,35 +1,17 @@
 package bufpipe_test
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/acomagu/bufpipe"
 )
 
-func Example1() {
+func Example() {
 	r, w := bufpipe.New(nil)
-	io.WriteString(w, "abc") // Don't blocks.
-	io.WriteString(w, "def") // Don't blocks, too.
-	w.Close()
-	io.Copy(os.Stdout, r)
-	// Output: abcdef
-}
-
-func Example2() {
-	r, w := bufpipe.New(nil)
-	result := make(chan []byte)
-	go func() {
-		s, _ := ioutil.ReadAll(r) // Blocks until the write half is closed.
-		result <- s
-	}()
+	go io.Copy(os.Stdout, r) // => abcdef.
 
 	io.WriteString(w, "abc")
 	io.WriteString(w, "def")
 	w.Close()
-
-	fmt.Println(string(<-result))
-	// Output: abcdef
 }
